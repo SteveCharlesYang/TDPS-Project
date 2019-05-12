56c5a8f01a16ebdcdfae53efc0e4db878b5e35f5
Charles Yang <Charles@std.uestc.edu.cn>
2019-05-12 22:27:41 +0800

    Fix pin settings for L298P.
    Fix pin settings for L298P.

--- Arduino/DualL298PMotorShield.h
+++ Arduino/DualL298PMotorShield.h
@@ -8,8 +8,8 @@ class DualL298PMotorShield
   public:  
     // CONSTRUCTORS
     DualL298PMotorShield(); // Default pin selection.
-    DualL298PMotorShield(unsigned char M1DIR, unsigned char M1PWM,
-                           unsigned char M2DIR, unsigned char M2PWM); // User-defined pin selection. 
+    DualL298PMotorShield(unsigned char M1DIR, unsigned char M1DIR2, unsigned char M1PWM,
+                           unsigned char M2DIR, unsigned char M2DIR2, unsigned char M2PWM); // User-defined pin selection. 
 
     // PUBLIC METHODS
     void init(); // Initialize TIMER 1, set the PWM to 20kHZ. 
@@ -18,10 +18,12 @@ class DualL298PMotorShield
     void setSpeeds(int m1Speed, int m2Speed); // Set speed for both M1 and M2.
 
   private:
-    static const unsigned char _M1DIR = 4;
-    static const unsigned char _M2DIR = 7;
-    static const unsigned char _M1PWM = 5;
-    static const unsigned char _M2PWM = 6;
+    static const unsigned char _M1DIR = 7;
+    static const unsigned char _M2DIR = 12;
+    static const unsigned char _M1DIR2 = 8;
+    static const unsigned char _M2DIR2 = 13;
+    static const unsigned char _M1PWM = 9;
+    static const unsigned char _M2PWM = 10;
 };
 
 #endif
